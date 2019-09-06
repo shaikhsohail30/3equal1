e37da533b3e32b693350794911abfab3420a2bee
Sohail Shaikh <shaikhsohail30@gmail.com>
2019-09-06 21:32:00 +0530

    REST API

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import sqlite3 as sql
 import pdb
 
@@ -12,7 +12,7 @@ def hello(user):
     return render_template('home.html', name=user)
 
 
-@app.route('/students')
+@app.route('/stud')
 def new_student():
    return render_template('student.html')
 
@@ -50,8 +50,69 @@ def list():
     cur = con.cursor()
     cur.execute("select * from students")
 
-    rows = cur.fetchall();
-    return render_template("list.html", rows=rows)
+    rows = cur.fetchall()
+    return render_template("bootstrap-list.html", rows=rows)
+
+
+@app.route('/students/api/v1.0/list', methods=['GET'])
+def get_students():
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+    cur.execute("select * from students")
+
+    rows = cur.fetchall()
+    students_list = []
+    for student in rows:
+        students_list.append(
+            {
+                "name": student['name'],
+                "address": student['addr'],
+                "city": student['city'],
+                "pincode": student['pin']
+            }
+        )
+    return jsonify({'students': students_list})
+
+
+@app.route('/student-post', methods=['POST'])
+def post_students():
+    if request.method == 'POST':
+        try:
+            nm = request.json['name']
+            addr = request.json['addr']
+            city = request.json['city']
+            pin = request.json['pin']
+
+            with sql.connect("database.db") as con:
+                cur = con.cursor()
+                cur.execute('''INSERT INTO students (name,addr,city,pin)
+                VALUES(?, ?, ?, ?)''', (nm, addr, city, pin))
+
+                con.commit()
+                msg = "Record successfully added"
+        except:
+            con.rollback()
+            msg = "error in insert operation"
+
+        finally:
+            cur = con.cursor()
+            cur.execute("select * from students")
+
+            rows = cur.fetchall()
+            students_list = []
+            for student in rows:
+                students_list.append(
+                    {
+                        "name": student[0],
+                        "address": student[1],
+                        "city": student[2],
+                        "pincode": student[3]
+                    }
+                )
+            con.close()
+    return jsonify({'students': students_list})
 
 
 if __name__ == '__main__':
